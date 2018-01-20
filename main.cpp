d69d0f2e7c1966a32489b79337358b84a6e1c8f6
kim sungkeun <rtr14@naver.com>
2018-01-20 16:42:06 +0900

    make main.cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* main.cpp
+ * Author	: Kim sungkeun
+ * Date		: 2018.01.20
+ *
+ * ## summary ##
+ * This file is main stream for start.
+ *
+ * 1. get data for image process. (get video data in use note-cam)
+ * 2. show the video.
+ */
+
+incldue <studio>
+
+
